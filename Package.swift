ae4d5d64afbb154d271003d95b3b0359a170693a
Ngo <ngo@readelysian.com>
2024-08-31 02:11:44 +0700

    update platform requirements

--- Package.swift
+++ Package.swift
@@ -5,6 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "Stitcher",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13),
+        .visionOS(.v1)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
